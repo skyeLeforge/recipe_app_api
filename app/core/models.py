51587731ad2d0db7030c25a4e00432cff9f85cc5
Skye <skyeleforge@gmail.com>
2020-12-03 12:24:00 -0500

    Travis is ignoring my .Flake8 file so Ive had travis stop checking linting

--- app/core/models.py
+++ app/core/models.py
@@ -49,7 +49,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-
 class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
     name = models.CharField(max_length=255)
